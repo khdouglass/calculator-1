bf2e4e57a0c22388faebfd84d630bb66bec94385
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 12:17:06 -0700

    Corrected division to float

--- arithmetic.py
+++ arithmetic.py
@@ -4,28 +4,28 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the difference of two numbers"""
-    pass
+    return num1 - num2
 
 def multiply(num1, num2):
     """Return the product of two numbers"""
-    pass
+    return num1 * num2
 
 def divide(num1, num2):
     """Return the quotient of two numbers as a float"""
-    pass
+    return float(num1 / num2)
 
 def square(num):
     """Return the square of a number"""
-    pass
+    return num * num
 
 def cube(num):
     """Return the cube of a number"""
-    pass
+    return num * num * num
 
 def power(num, exponent):
     """Return num raised to the power of exponent"""
-    pass
+    return num ** exponent
 
 def mod(num1, num2):
     """Return remainder of num1 divided by num2"""
-    pass
+    return num1 % num2
